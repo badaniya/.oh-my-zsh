314e0528abbabf5ad54482faeebdef352fc9b61b
badaniya <badaniya@extremenetworks.com>
2024-07-06 21:42:01 -0700

    fzf: Update shell script helps to use HOME env instead.

--- fzf/.fzf.bash
+++ fzf/.fzf.bash
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/badaniya/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/badaniya/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 eval "$(fzf --bash)"
