de79d009407709b85200844b45f5a201e40ca4f7
badaniya <badaniya@extremenetworks.com>
2024-09-16 19:08:46 -0700

    nvim: Update obsidian plug-in configuration to disable ui markdown rendering

--- nvim/.config/nvim/lua/custom/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/custom/plugins/obsidian.lua
@@ -202,7 +202,7 @@ return {
       ---@param img string
       follow_img_func = function(img)
         -- vim.fn.jobstart { 'qlmanage', '-p', img } -- Mac OS quick look preview
-        vim.fn.jobstart { 'xdg-open', url } -- linux
+        vim.fn.jobstart { 'xdg-open', img } -- linux
         -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
       end,
 
@@ -276,8 +276,10 @@ return {
 
       -- Optional, configure additional syntax highlighting / extmarks.
       -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
+      conceallevel = 1,
+
       ui = {
-        enable = true, -- set to false to disable all additional syntax features
+        enable = false, -- set to false to disable all additional syntax features
         update_debounce = 200, -- update delay after a text change (in milliseconds)
         max_file_length = 5000, -- disable UI features for files with more than this many lines
         -- Define how various check-boxes are displayed
