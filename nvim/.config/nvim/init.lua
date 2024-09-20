fa09673de1e481155abbc14101d792ceae997360
badaniya <badaniya@extremenetworks.com>
2024-09-19 21:32:11 -0700

    nvim: Add various LSP support including markdown, yaml, toml, json, docker, bash ...

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -499,7 +499,22 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        -- rust_analyzer = {},
+        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        --
+        -- Some languages (like typescript) have entire language plugins that can be useful:
+        --    https://github.com/pmizio/typescript-tools.nvim
+        --
+        -- But for many setups, the LSP (`tsserver`) will work just fine
+        -- tsserver = {},
+
+        -- bash LSP
+        bashls = {},
+        -- gRPC protobufs LSP
+        bufls = {},
+        -- C/C++ LSP
+        clangd = {},
+        -- go LSP
         gopls = {
           settings = {
             gopls = {
@@ -511,17 +526,13 @@ require('lazy').setup({
             },
           },
         },
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        -- graphql LSP
+        graphql = {},
+        -- helm chart lsp
+        helm_ls = {},
+        -- jq LSP
+        jqls = {},
+        -- lua LSP
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -536,6 +547,20 @@ require('lazy').setup({
             },
           },
         },
+        -- markdown LSP
+        marksman = {},
+        -- nix/nix flakes LSP
+        nil_ls = {},
+        -- python LSP
+        pyright = {},
+        -- JSON/YAML LSP
+        spectral = {},
+        -- SQL LSP
+        sqls = {},
+        -- TOML LSP
+        taplo = {},
+        -- openapi YAML LSP
+        vacuum = {},
       }
 
       -- Ensure the servers and tools above are installed
