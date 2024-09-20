1aa412263c31c35fd23d22d114b27fb2751d3343
badaniya <badaniya@extremenetworks.com>
2024-09-19 21:35:12 -0700

    nvim: Obsidian config dir setup

--- nvim/.config/nvim/lua/custom/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/custom/plugins/obsidian.lua
@@ -1,16 +1,18 @@
 return {
   'epwalsh/obsidian.nvim',
-  version = '*', -- recommended, use latest release instead of latest commit
+  version = 'v3.9.0', -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = 'markdown',
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    -- refer to `:h file-pattern` for more examples
+    -- 'BufReadPre '
+    --   .. vim.fn.expand '~'
+    --   .. 'vaults/**/*.md',
+    -- 'BufNewFile ' .. vim.fn.expand '~' .. 'vaults/**/*.md',
+  },
   dependencies = {
     -- Required.
     'nvim-lua/plenary.nvim',
@@ -29,10 +31,6 @@ return {
       -- current markdown file being edited.
       workspaces = {
         {
-          name = 'personal',
-          path = '~/vaults/personal',
-        },
-        {
           name = 'work',
           path = '~/vaults/work',
           -- Optional, override certain settings.
@@ -44,10 +42,10 @@ return {
 
       -- Alternatively - and for backwards compatibility - you can set 'dir' to a single path instead of
       -- 'workspaces'. For example:
-      -- dir = "~/vaults/work",
+      -- dir = '~/vaults/work',
 
       -- Optional, if you keep notes in a specific subdirectory of your vault.
-      notes_subdir = 'notes',
+      -- notes_subdir = 'notes',
 
       -- Optional, set the log level for obsidian.nvim. This is an integer corresponding to one of the log
       -- levels defined by "vim.log.levels.*".
@@ -150,7 +148,7 @@ return {
       end,
 
       -- Either 'wiki' or 'markdown'.
-      preferred_link_style = 'wiki',
+      preferred_link_style = 'markdown',
 
       -- Optional, boolean or a function that takes a filename and returns a boolean.
       -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
@@ -276,10 +274,9 @@ return {
 
       -- Optional, configure additional syntax highlighting / extmarks.
       -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
-      conceallevel = 1,
 
       ui = {
-        enable = false, -- set to false to disable all additional syntax features
+        enable = true, -- set to false to disable all additional syntax features
         update_debounce = 200, -- update delay after a text change (in milliseconds)
         max_file_length = 5000, -- disable UI features for files with more than this many lines
         -- Define how various check-boxes are displayed
@@ -347,5 +344,7 @@ return {
         end,
       },
     }
+
+    vim.opt.conceallevel = 1
   end,
 }
